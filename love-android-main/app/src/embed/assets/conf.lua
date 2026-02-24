771a9ef7642bc48ca0032514513b52cfc214755f
zormneutron-collab <zormneutron@gmail.com>
2026-02-24 06:01:43 +0200

    Add files via upload

---
+++ love-android-main/app/src/embed/assets/conf.lua
@@ -0,0 +1,12 @@
+function love.conf(t)
+    t.identity = "juice_game"
+    t.modules.joystick = false
+    t.modules.physics = false
+    t.externalstorage = true
+    t.version = "12.0"
+    t.window.display = 0
+    t.window.fullscreen = true
+    t.window.fullscreentype = "desktop"
+    t.window.width = 0
+    t.window.height = 0
+end

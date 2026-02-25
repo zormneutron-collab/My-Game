6faf22e4f2fd5218c202161ebc3b5b64f1b4bfa1
zormneutron-collab <zormneutron@gmail.com>
2026-02-25 03:57:29 +0200

    Update GameActivity.java

--- love-android-main/app/src/main/java/org/love2d/android/GameActivity.java
+++ love-android-main/app/src/main/java/org/love2d/android/GameActivity.java
@@ -1,5 +1,6 @@
 package org.love2d.android;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
@@ -17,6 +18,8 @@ import android.view.WindowManager;
 
 import androidx.annotation.Keep;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
 import org.libsdl.app.SDLActivity;
 
 import java.io.FileNotFoundException;
@@ -36,6 +39,7 @@ public class GameActivity extends SDLActivity {
 
     private static final String TAG        = "GameActivity";
     private static final String ADTAG      = "UnityAdsGame";
+    public  static final int RECORD_AUDIO_REQUEST_CODE = 3;
 
     private static final String GAME_ID    = "6051973";
     private static final String AD_UNIT_ID = "Rewarded_Android";
@@ -55,6 +59,7 @@ public class GameActivity extends SDLActivity {
 
     protected Vibrator vibrator;
     protected boolean  shortEdgesMode;
+    protected final int[] recordAudioRequestDummy = new int[1];
     private Uri      delayedUri = null;
     private String[] args;
     private boolean  isFused;
@@ -317,7 +322,16 @@ public class GameActivity extends SDLActivity {
     public void onRequestPermissionsResult(int requestCode,
                                            String[] permissions,
                                            int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0) {
+            if (requestCode == RECORD_AUDIO_REQUEST_CODE) {
+                synchronized (recordAudioRequestDummy) {
+                    recordAudioRequestDummy[0] = grantResults[0];
+                    recordAudioRequestDummy.notify();
+                }
+            } else {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
+        }
     }
 
     @Keep
@@ -413,6 +427,27 @@ public class GameActivity extends SDLActivity {
     @Keep
     public boolean getImmersiveMode() { return shortEdgesMode; }
 
+    @Keep
+    public boolean hasRecordAudioPermission() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+               == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Keep
+    public void requestRecordAudioPermission() {
+        if (hasRecordAudioPermission()) return;
+        ActivityCompat.requestPermissions(this,
+            new String[]{Manifest.permission.RECORD_AUDIO},
+            RECORD_AUDIO_REQUEST_CODE);
+        synchronized (recordAudioRequestDummy) {
+            try {
+                recordAudioRequestDummy.wait();
+            } catch (InterruptedException e) {
+                Log.d(TAG, "mic wait interrupted", e);
+            }
+        }
+    }
+
     public int getAudioSMP() {
         int smp = 256;
         AudioManager a = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
@@ -490,4 +525,4 @@ public class GameActivity extends SDLActivity {
             else if (scheme.equals("file")) args = new String[]{path};
         }
     }
-}
+                        }
